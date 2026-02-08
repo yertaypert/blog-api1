9c3bbd793c90a7af3a798f47009962b3714541a6
star <zhambulaie@gmail.com>
2026-02-08 23:05:43 +0500

    deleted unnecessary app

--- myapp/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-
-from . import views
-
-urlpatterns = [
-    path('', views.hello, name='hello'),
-    path("simple_get/", views.my_view, name='simple_get_view'),
-]  
