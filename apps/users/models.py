a30c0a570d3650b33464ea3bdcfa7391d42e2256
star <zhambulaie@gmail.com>
2026-02-16 16:01:20 +0500

    little fixes

--- apps/users/models.py
+++ apps/users/models.py
@@ -53,14 +53,14 @@ class User(AbstractBaseUser, PermissionsMixin):
 
     groups = models.ManyToManyField(
         'auth.Group',
-        related_name='custom_user_set',  # <--- change this
+        related_name='custom_user_set',
         blank=True,
         help_text='The groups this user belongs to.',
         verbose_name='groups',
     )
     user_permissions = models.ManyToManyField(
         'auth.Permission',
-        related_name='custom_user_set_permissions',  # <--- change this
+        related_name='custom_user_set_permissions',
         blank=True,
         help_text='Specific permissions for this user.',
         verbose_name='user permissions',
--- settings/conf.py
+++ settings/conf.py
@@ -2,10 +2,10 @@ import os
 from decouple import config, Config, RepositoryEnv
 
 
-# 1. Get the absolute path to the directory containing this file (settings/)
+
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
-# 2. Point specifically to the .env file inside this folder
+
 env_path = os.path.join(BASE_DIR, '.env')
 config = Config(RepositoryEnv(env_path))
 
