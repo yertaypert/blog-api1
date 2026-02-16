7b7da5ef5ac00ae083673c143d57107f1c1c9671
star <zhambulaie@gmail.com>
2026-02-16 15:14:47 +0500

    register endpoint fix

--- apps/users/urls.py
+++ apps/users/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from .views import RegisterView
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
@@ -6,6 +7,6 @@ from rest_framework_simplejwt.views import (
 
 
 urlpatterns = [
-    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('register/', RegisterView.as_view({'post': 'create'}), name='register'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
--- settings/urls.py
+++ settings/urls.py
@@ -7,6 +7,7 @@ from rest_framework_simplejwt.views import TokenObtainPairView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("api/auth/", include("apps.users.urls")),
     path('api/auth/token/', 
          ratelimit(key='ip', rate='10/m', method='POST', block=True)(TokenObtainPairView.as_view()), 
          name='token_obtain_pair'),
